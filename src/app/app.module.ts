85eb817e71c2b6f2c58ae2547c72eaafbed2a99e
AYUSH YADAV <ayushexx@gmail.com>
2022-05-22 11:39:53 +0530

    d3-wc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ChartModule } from '@smart-webcomponents-angular/chart';
+import { AccordionModule } from 'smart-webcomponents-angular/accordion';
+
 import { AppComponent } from './app.component';
+import 'd3stencilproject';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, ChartModule, AccordionModule],
+  // exports: [Components],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
